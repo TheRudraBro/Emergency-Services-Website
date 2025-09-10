8e5b23eae681f3aa6466c61534bbf083f3c92518
TheRudraBro <rudramraj22@gmail.com>
2025-09-10 23:22:45 +0600

    call function updated

--- styles/index.js
+++ styles/index.js
@@ -27,3 +27,47 @@ for (let copy of copys) {
     navigator.clipboard.writeText(CardNumber);
   });
 }
+
+// call button functionality
+let callBtns = document.getElementsByClassName("callBtn");
+
+for (let callBtn of callBtns) {
+  callBtn.addEventListener("click", function () {
+    let cardHeadline =
+      callBtn.parentNode.parentNode.children[1].children[0].innerText;
+    let CardSubTitle =
+      callBtn.parentNode.parentNode.children[1].children[1].innerText;
+    let CardNumber =
+      callBtn.parentNode.parentNode.children[1].children[2].innerText;
+
+    let coinNumber = document.getElementById("coinNum");
+    let coinNumberConvert = Number(coinNumber.innerText);
+
+    if (coinNumberConvert < 20) {
+      alert(`❌ You don't have enough coin; Need atleast 20 coin to call.`);
+      return;
+    }
+
+    let coinDecrease = coinNumberConvert - 20;
+    coinNumber.innerText = coinDecrease;
+
+    let currentTime = new Date();
+    let getTime = currentTime.toLocaleTimeString();
+
+    alert(`📞 calling ${cardHeadline} ${CardNumber}...`);
+
+    let callListBoard = document.getElementById("callHistory");
+    let div = document.createElement("div");
+    div.className = `bg-[#FAFAFA] border border-transparent rounded-lg px-6 py-4 mb-4 shadow-sm flex flex-row items-center justify-between`;
+    div.innerHTML = `
+    <div class="flex flex-col gap-1">
+       <h1 class="inter-a text-lg font-semibold">${cardHeadline}</h1>
+       <p class="hind-madurai text-lg font-normal">${CardNumber}</p>  
+    </div>
+    <div>
+    <h1 class="hind-madurai text-lg font-normal">${getTime}</h1>
+    </div>`;
+
+    callListBoard.appendChild(div);
+  });
+}
